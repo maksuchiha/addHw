947dd709f821d3d8215b755a5b7d1f2f44c5a2a1
Maksim Sheko <sakusan@mail.ru>
2022-10-13 23:35:53 +0300

    add build in project

---
+++ build/precache-manifest.259ce671d65a897e1ebdb711f3299cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cb7eaa9709315ac6644ea6c6464ba2",
+    "url": "/addHw/index.html"
+  },
+  {
+    "revision": "a01196e43f98604b3549",
+    "url": "/addHw/static/css/main.a44de9fe.chunk.css"
+  },
+  {
+    "revision": "e3943099cfc42689d3d7",
+    "url": "/addHw/static/js/2.f8a5e7b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/addHw/static/js/2.f8a5e7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01196e43f98604b3549",
+    "url": "/addHw/static/js/main.373bfa0f.chunk.js"
+  },
+  {
+    "revision": "c9af6fccf5f3d800e2a1",
+    "url": "/addHw/static/js/runtime-main.9783898d.js"
+  }
+]);
