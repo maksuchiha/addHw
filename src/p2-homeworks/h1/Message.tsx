38c5967239d530a85902958a303dc5de2bc9aeaa
Maksim Sheko <sakusan@mail.ru>
2022-10-13 23:33:40 +0300

    task 1 complete

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import s from './Message.module.css'
+
+
+type propsType = {
+    data: {
+        avatar: string
+        name: string
+        message: string
+        time: string
+    }
+}
+
+function Message(props: propsType) {
+    return (
+        <div className={s.message}>
+            <div className={s.message__img}>
+                <img src={props.data.avatar} alt=""/>
+            </div>
+            <div className={s.messageItem}>
+                <div className={s.messageItem__info}>
+                    <div className={s.messageItem__name}>{props.data.name}</div>
+                    <div className={s.messageItem__message}>{props.data.message}</div>
+                </div>
+                <div className={s.messageItem__time}>
+                    {props.data.time}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
